9a4e31905e0fb24cdac6f7d34b61ec29351bd99e
Samuel Bourassa <district67@gmail.com>
2023-03-16 14:52:32 -0400

    Implement the viewModel.

--- app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -15,6 +15,7 @@
  */
 package com.example.forage.data
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -30,24 +31,18 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ForageableDao {
 
-    // TODO: implement a method to retrieve all Forageables from the database
     @Query("Select * from forageable_database")
-    suspend fun getForageables(): Flow<List<Forageable>>
+    fun getForageables(): Flow<List<Forageable>>
 
-    // TODO: implement a method to retrieve a Forageable from the database by id
     @Query("Select * from forageable_database Where id = :id")
-    suspend fun getForageable(id: Int)
+    fun getForageable(id: Long): Flow<Forageable>
 
-    // TODO: implement a method to insert a Forageable into the database
-    //  (use OnConflictStrategy.REPLACE)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(forageable: Forageable)
 
-    // TODO: implement a method to update a Forageable that is already in the database
     @Update
     suspend fun update(forageable: Forageable)
 
-    // TODO: implement a method to delete a Forageable from the database.
     @Delete
     suspend fun delete(forageable: Forageable)
 }
